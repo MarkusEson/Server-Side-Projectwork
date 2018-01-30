e63fe2d47d0d19ceea1d75512cfba43575158e6c
Unknown <markus.eson@hotmail.com>
2018-01-30 13:00:55 +0100

    Added LIST and changed Model Name

--- Server-Side Projectwork/Controllers/LibraryController.cs
+++ Server-Side Projectwork/Controllers/LibraryController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Server_Side_Projectwork.Models;
 
 namespace Server_Side_Projectwork.Controllers
 {
     public class LibraryController : Controller
     {
+
+        // Instead of a database, we use a static list.
+        public static List<Book> Books = new List<Book>{
+            new Book { ISBN = 123, Author = "Markus" },
+            new Book { ISBN = 184, Author = "Sukram" }
+        };
+
+
         // GET: Library
         public ActionResult Index()
         {
