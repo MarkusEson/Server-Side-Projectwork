a96844e4316eb8fa38594240b658e5fe223d786a
Unknown <markus.eson@hotmail.com>
2018-01-30 20:09:00 +0100

    Ändrade Model, la till Böcker.

--- Server-Side Projectwork/Controllers/LibraryController.cs
+++ Server-Side Projectwork/Controllers/LibraryController.cs
@@ -12,19 +12,16 @@ namespace Server_Side_Projectwork.Controllers
 
         // Instead of a database, we use a static list.
         public static List<Book> Books = new List<Book>{
-   
-            new Book { Title = "Radiology and You", Author = "Marie Curie", ISBN = 833 },
-            new Book { Title = "How to Drive and Sing", Author = "James Corden", ISBN = 042 },
-            new Book { Title = "Veggies for Cooking", Author = "Artie Choke", ISBN = 112 },
-            new Book { Title = "Proper Pronounciation", Author = "Benediff Cucumberbatch", ISBN = 399 },
-            new Book { Title = "Yxskaft", Author = "Ralun Jorge", ISBN = 345 },
-            new Book { Title = "Dålig Musik", Author = "Leffe Hoffmeistro", ISBN = 216 },
-            new Book { Title = "Konstiga Namn", Author = "Solen Gårupp", ISBN = 987 },
-            new Book { Title = "Filmologi", Author = "Dum Dummare", ISBN = 222 },
-            new Book { Title = "Vem är det?", Author = "Markus Eriksson", ISBN = 100 }
-
+            new Book { Title = "Radiology and You",         Author = "Marie Curie",             ISBN = 833 },
+            new Book { Title = "How to Drive and Sing",     Author = "James Corden",            ISBN = 042 },
+            new Book { Title = "Veggies for Cooking",       Author = "Artie Choke",             ISBN = 112 },
+            new Book { Title = "Proper Pronounciation",     Author = "Benediff Cucumberbatch",  ISBN = 399 },
+            new Book { Title = "Yxskaft",                   Author = "Ralun Jorge",             ISBN = 345 },
+            new Book { Title = "Dålig Musik",               Author = "Leffe Hoffmeistro",       ISBN = 216 },
+            new Book { Title = "Konstiga Namn",             Author = "Solen Gårupp",            ISBN = 987 },
+            new Book { Title = "Filmologi",                 Author = "Dum Dummare",             ISBN = 222 },
+            new Book { Title = "Vem är det?",               Author = "Markus Eriksson",         ISBN = 100 }
         };
-
 
         // GET: Library
         public ActionResult Index()
