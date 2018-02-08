d18a568e85a27b2409b740983b2a8c0118e47eac
Unknown <markus.eson@hotmail.com>
2018-02-08 09:05:39 +0100

    Added Controllers, and fixed Views
    Added Book, autor and lib controller.
    fixed view of books.

---
+++ Server-Side Projectwork/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Server_Side_Projectwork.Models;
+
+namespace Server_Side_Projectwork.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Library
+        public ActionResult Index()
+        {
+            Repository repo = new Repository();
+            Session["repo"] = repo;
+
+            return View();
+        }
+
+        // GET: AddBook
+        [HttpGet]
+        public ActionResult AddBook()
+        {
+            return View("AddBook");
+        }
+
+        // POST: AddBook - adds it to list
+        [HttpPost]
+        public ActionResult AddBook(Book book)
+        {
+            Repository repo = (Repository)Session["repo"];
+            repo.BookList.Add(book);
+            return Redirect("Books");
+        }
+
+        [HttpGet]
+        public ActionResult ShowBook(int id)
+        {
+            Repository repo = (Repository)Session["repo"];
+
+            //return View("ShowBook", repo.BookList[id]);
+            return View("ShowBook", repo.BookList.Find(x => (x.ISBN == id)));
+        }
+    }
+}
