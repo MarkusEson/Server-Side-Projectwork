2cdc860f079b8d2db8ae8711e74fdb271513038f
Pontus <datapontus@hotmail.com>
2018-06-06 11:50:39 +0200

    - Delete current account fixed

--- Server-Side Projectwork/Controllers/AdminController.cs
+++ Server-Side Projectwork/Controllers/AdminController.cs
@@ -50,10 +50,10 @@ namespace Server_Side_Projectwork.Controllers
         public ActionResult Create(Administrator newAdmin)
         {
             
-            bool isAuthorized = Administrator.IsAuthorized((int?)(Session["UserID"] ?? null), (int?)(Session["UserRank"] ?? null), (int)Authorization.Rank.administrator);
+            //bool isAuthorized = Administrator.IsAuthorized((int?)(Session["UserSession"] ?? null), (int?)(Session["UserRank"] ?? null), (int)Authorization.Rank.administrator);
 
-            if(isAuthorized) //"Auth"
-            {
+            //if(isAuthorized) //"Auth"
+            //{
                 foreach (var admin in Administrator.GetAdminList())
                 {
                     if (admin.UserName == newAdmin.UserName)
@@ -79,14 +79,14 @@ namespace Server_Side_Projectwork.Controllers
         [HttpPost]
         public ActionResult Delete(int id)
         {
-            Administrator.DeleteAdmin(id);
-
             if(Session["UserSession"].Equals( Administrator.GetAdmin(id).UserName )) // Check if the deleted admin is currently logged in
             {
                 Session.Abandon();
                 Session.Contents.Abandon();
                 Session.Contents.RemoveAll();
             }
+
+            Administrator.DeleteAdmin(id);
 
             return RedirectToAction("Index");
         }
